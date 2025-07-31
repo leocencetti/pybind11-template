71605890fbadcab559facef4e8c9c6e2b86b25f0
Leonardo Cencetti <leonardo.cencetti@flybotix.com>
2025-07-31 17:16:18 +0200

    chore: Build bindings with pip

--- cmake/FetchContent_Declare_pybind11.cmake
+++ cmake/FetchContent_Declare_pybind11.cmake
@@ -1,10 +1,12 @@
 if(LOCAL_REPOSITORIES_BASE_DIR)
-  FetchContent_Declare(pybind11 SOURCE_DIR
-                                ${LOCAL_REPOSITORIES_BASE_DIR}/pybind11)
+  FetchContent_Declare(
+    pybind11 SOURCE_DIR ${LOCAL_REPOSITORIES_BASE_DIR}/pybind11
+             OVERRIDE_FIND_PACKAGE ON)
 else()
   FetchContent_Declare(
     pybind11
     GIT_REPOSITORY https://github.com/pybind/pybind11.git
-    GIT_TAG v2.10.0
-    GIT_SHALLOW ON)
+    GIT_TAG v3.0.0
+    GIT_SHALLOW ON
+    OVERRIDE_FIND_PACKAGE ON)
 endif()
